f69e4ae93c3541ceec7d02181ba722f782ecb71f
Nicolas Martignole <nicolas@martignole.net>
2014-08-01 07:59:25 +0200

    Form : ajout d'une page de création et une action de validation/sauvegarde.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,59 +7,76 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import models.Bar;
 import models.Entry;
-import play.*;
 import play.data.Form;
 import play.db.ebean.Model;
 import play.libs.Json;
 import play.mvc.*;
 import services.BarService;
-import views.html.*;
 
 public class Application extends Controller {
-	
 
     @Autowired
     static BarService barService;
 
     public static Result index() {
-        return ok(index.render("Some String param can eventually appear.."));
+        return ok(views.html.index.render("Some String param can eventually appear.."));
     }
-    
+
     public static Result cpage1(String name, int age) {
-    	String s = "Extended Tiger Running Space. cpage1";
-        return ok(vpage1.render(name, age));
+        String s = "Extended Tiger Running Space. cpage1";
+        return ok(views.html.vpage1.render(name, age));
     }
-    
+
     public static Result displayObject() {
-    	ArrayList <Entry> entries = new ArrayList<Entry>();
-    	Entry entry1 = new Entry();
-    	Entry entry2 = new Entry();
-    	entry1.m1 = "entry from m1";
-    	entry2.m2 = "entry from m2";
-    	entries.add(entry1);
-    	entries.add(entry2);
-    	return ok(
-    	    objectRenderer.render(entries)
-    	);
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+        Entry entry1 = new Entry();
+        Entry entry2 = new Entry();
+        entry1.m1 = "entry from m1";
+        entry2.m2 = "entry from m2";
+        entries.add(entry1);
+        entries.add(entry2);
+        return ok(
+                views.html.objectRenderer.render(entries)
+        );
     }
-    
+
     public static Result cpage3() {
-        return ok(vpage3.render("plop"));
+        return ok(views.html.vpage3.render("plop"));
     }
-    
-    public static Result addBar() {
-        Form<Bar> form = Form.form(Bar.class).bindFromRequest();
+
+    public static Result addBar(){
         //Bar bar = Form.form(Bar.class).bindFromRequest().get();
         //barService.addBar(bar);
-        Bar bar = form.get();
-        bar.save();
+//        Bar bar = form.get();
+//        bar.save();
         return play.mvc.Controller.redirect(controllers.routes.Application.cpage3());
+    }
+
+    public static Result createNewBarAction() {
+        Form<Bar> form = Form.form(Bar.class);
+        return ok(views.html.createNewBar.render(form));
+    }
+
+    public static Result saveBar() {
+        Form<Bar> form = Form.form(Bar.class).bindFromRequest();
+        if (form.hasErrors()) {
+            return badRequest(views.html.createNewBar.render(form)); // display the form with errors
+        } else {
+            Bar bar = form.get();
+            bar.save();
+            flash("success",String.format("New bar %s has been created",bar.name));
+            return play.mvc.Controller.redirect(controllers.routes.Application.cpage3());
+        }
     }
 
     public static Result getBars() {
         //return play.mvc.Controller.ok(Json.toJson(barService.getAllBars()));
-    	List<Bar> bars = new Model.Finder(String.class, Bar.class).all();
-    	return ok(Json.toJson(bars));
+        List<Bar> bars = new Model.Finder(String.class, Bar.class).all();
+
+        // JAVA - content type est mis automatiquement
+        // voir http://www.playframework.com/documentation/2.3.x/JavaResponse
+        return ok(Json.toJson(bars));
+
     }
 
 }
--- app/models/Bar.java
+++ app/models/Bar.java
@@ -3,6 +3,7 @@ package models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
 @Entity
@@ -10,7 +11,13 @@ public class Bar extends Model {
 	
 	@Id
 	public String id;
-	
+
+    @Constraints.Required
 	public String name;
 
+    //@Constraints.Email
+    //public String email;
+
+    //@Constraints.MinLength(8)
+    //public String password;
 }
